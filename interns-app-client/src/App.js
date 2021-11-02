4920ec267a1b20d3186938a3cca317efcd6d5b81
jorge <jorje.elduro3@gmail.com>
2021-11-02 08:34:13 -0400

    so

--- interns-app-client/src/App.js
+++ interns-app-client/src/App.js
@@ -1,12 +1,16 @@
 import HeaderAppRegistration from "./components/app/HeaderAppRegistration";
+import login from './pages/login'
+import Menu from './components/landing/register/Menu'
+import HomeRegister from "./components/landing/register/HomeRegister";
+import GeneralInformation from './components/landing/register/GeneralInformation'
+
 
 
 function App() {
   
   return (
   <>
-      <HeaderAppRegistration/>
-      <h1>hola mundo desde el app</h1>
+      <GeneralInformation/>
    </>
   );
 }
